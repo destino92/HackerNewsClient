41e12eeff7566d3819789c8a2a646129e2c13b0f
destino92 <destinodello@gmail.com>
2017-02-13 18:54:46 +0300

    created a Button components and render it inside Table component

--- src/App.js
+++ src/App.js
@@ -69,6 +69,27 @@ class App extends Component {
   }
 }
 
+class Button extends Component {
+  render() {
+    const {
+      onClick,
+      className,
+      children,
+    } = this.props;
+
+    return (
+      <button
+        onClick={onClick}
+
+        className=''
+        type="button"
+      >
+        {children}
+      </button>
+    );
+  }
+}
+
 class Search extends Component {
   render() {
     const { value, onChange, children } = this.props;
@@ -98,11 +119,9 @@ class Table extends Component {
               <span>{item.num_comments}</span>
               <span>{item.points}</span>
               <span>
-                <button
-                  onClick={() => onDismiss(item.objectID)}
-                  type="button">
-                    Dismiss
-                </button>
+                <Button onClick={() => onDismiss(item.objectID)}>
+                  Dismiss
+                </Button>
               </span>
             </div>
         )}
