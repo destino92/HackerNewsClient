f0028c223e4e36a50d91e8075d3082ead308b5f0
destino92 <destinodello@gmail.com>
2017-02-16 17:07:37 +0300

    test: added snapshots for Search component test

--- src/App.test.js
+++ src/App.test.js
@@ -24,5 +24,13 @@ describe('Search', () => {
   it('renders', () => {
     const div = document.createElement('div');
     ReactDOM.render(<Search>Search</Search>, div);
-  })
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
