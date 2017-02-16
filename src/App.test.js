e4028f2d04472b11abfde118904c535a18784dba
destino92 <destinodello@gmail.com>
2017-02-16 17:18:33 +0300

    test: added test to see if Table component renders

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App, { Search, Button } from './App';
+import App, { Search, Button, Table } from './App';
 
 describe('App', () => {
 
@@ -44,6 +44,28 @@ describe('Button', () => {
   test('snapshots', () => {
     const component = renderer.create(
       <Button>Give me more</Button>
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+describe('Table', () => {
+  const props = {
+    list: [
+      {title: '1', author: '1', num_comments: 1, points: 2, objectID: 'y'},
+      {title: '2', author: '2', num_comments: 1, points: 2, objectID: 'z'}
+    ],
+  };
+
+  it('renders', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Table { ...props } />, div);
+  });
+
+  test('snapshots', () => {
+    const component = renderer.create(
+      <Table { ...props } />
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
